e7df37cc77c098eb4febba41661e2cd581e4acd2
Dmitriy Ignatiev <dmitriy.ignatiev83@gmail.com>
2019-12-02 18:25:49 +0300

    'first_vue'

--- suppliers/static/invtocust.js
+++ suppliers/static/invtocust.js
@@ -2,3 +2,21 @@ function addInvFormtoCust(){
    const utable=document.getElementById('formInvcust')
    utable.style.display='block'
 }
+
+$("#addserviceb").bind("click", function(){
+   
+   const utextarea = document.createElement('textarea')
+   utextarea.setAttribute("class","form-control")
+  
+   var textnode = document.createTextNode("Water")
+   utextarea.appendChild(textnode) 
+   console.log(utextarea)
+   const parrent = document.getElementById("addservicef")
+   const up=documen.createElement('p')
+   
+   console.log(parrent)
+   parrent.appendChild(up)
+   up.appendChild(utextarea)
+})
+   
+
